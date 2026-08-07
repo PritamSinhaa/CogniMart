fac1f75e92ccd9886a37f9de7a807d2280041de7
PritamSinhaa <pritamsinha2323548@gmail.com>
2026-08-07 15:02:55 +0530

    chore: initialize frontend project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <h1 className="text-5xl font-bold text-center pt-20">
+        CogniMart
+      </h1>
+    </div>
+  );
+}
+
+export default App;
